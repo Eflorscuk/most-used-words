b1124f6c746b9461862f1c29f5d10a08591e0939
Evandro Florscuk <evandro_ff@me.com>
2023-03-12 12:09:00 -0300

    Modification of application window title

--- src/background.js
+++ src/background.js
@@ -27,12 +27,15 @@ async function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+
+  const {title, version} = require("../package.json")
+  win.setTitle(`${title} ${version}`)
 }
 
 // Quit when all windows are closed.
