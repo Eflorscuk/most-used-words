9daaadf02478e84bba4ec3871a5b1b5ba249d8d4
Evandro Florscuk <evandro_ff@me.com>
2023-03-13 15:09:45 -0300

    Vue graphical interface completed

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="pills">
-    <PillComponent v-for="item in groupedWords" :word="item.word" 
-    :amount="item.amount" :key="item.word"></PillComponent>
+  <div class="home">
+    <v-form>
+      <v-file-input
+      label="Selecione as legendas"
+      prepend-icon="mdi-message-text"
+      append-icon="mdi-send"
+      outlined
+      multiple
+      v-model="files"
+      @click:append="processSubtitles"
+      >
+
+      </v-file-input>
+    </v-form>
+    <div class="pills">
+      <PillComponent v-for="item in groupedWords" :word="item.word" 
+      :amount="item.amount" :key="item.word"></PillComponent>
+    </div>
   </div>
 </template>
 
@@ -13,18 +28,30 @@ export default {
     },
     data: function(){
         return {
+            files: [],
             groupedWords: [
                 { word: "i", amount: 547 },
                 { word: "you", amount: 478 },
                 { word: "it", amount: 1000 }
             ],
         }
+    },
+    methods: {
+      processSubtitles(){
+        console.log(this.files)
+      }
     }
 }
 </script>
 
 <style scoped>
+.home {
+  margin: 20px;
+}
+
 .pills {
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
